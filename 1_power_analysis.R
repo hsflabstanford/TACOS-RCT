76a289cc983c43c497059c1ecdd1d7984e074580
Seth Green <setgree@gmail.com>
2025-01-03 16:55:59 -0500

    ok I finally feel like I understand what this script is doing. and I do think we shold be able to get a sample size from it.

--- 1_power_analysis.R
+++ 1_power_analysis.R
@@ -1,6 +1,5 @@
 library(assertthat)
 library(bcaboot)
-library(boot)
 library(dplyr, warn.conflicts = FALSE)
 library(estimatr)
 library(fabricatr)
@@ -74,85 +73,41 @@ simulate_H2 = function(
   return(sims)
 }
 
-# XXX Broken Still raises an cryptic error:
-# Error in bca.ci(boot.out, conf, index[1L], L = L, t = t.o, t0 = t0.o,  :
-#                  estimated adjustment 'a' is NA
-#####################################
-# Power analysis of H2 using `boot` #
-#####################################
-H2_Specificity_CI_1 = function(data, N_bootstraps) {
-
-  # Helper function that generates the point estimate of the intervention effect
-  calculate_statistics_1 = function(data, bootstrap_sample_index) {
-    data = data[bootstrap_sample_index,]
-
-    Chicken_model = lm_robust(meat_outcome_chicken_binary ~ arm, data,
-                              se_type = 'HC0')
-    Meat_model = lm_robust(meat_outcome_not_chicken_binary ~ arm, data,
-                           se_type = 'HC0')
-
-    return(c(Chicken_mean = Chicken_model$coefficients['arm'],
-             Chicken_var = Chicken_model$vcov['arm','arm'],
-
-             Meat_mean = Meat_model$coefficients['arm'],
-             Meat_var = Meat_model$vcov['arm','arm'],
-
-             Chicken_minus_Meat = Chicken_model$coefficients['arm'] -
-               Meat_model$coefficients['arm'])
-           )
-  }
-
-  bs = boot::boot(data, calculate_statistics_1, R=N_bootstraps, parallel='multicore')
-
-  # Index is getting the point estimate (5) and standard error (2) via
-  # `calculate_statistics`
-  bs_ci = boot::boot.ci(bs, type="bca", index=c(5,2))
-
-  return(c(
-    # Get upper and lower bounds for a 95% CI
-    H2_Specificity_CI_lo=bs_ci$perc[4],
-    H2_Specificity_CI_hi=bs_ci$perc[5])
-  )
-
-}
-
-
-
 ########################################
 # Power analysis of H2 using `bcaboot` #
 ########################################
-# Helper function that generates the point estimate of the intervention effect
-calculate_statistics_2 = function(data) {
-  Chicken_model = estimatr::lm_robust(meat_outcome_chicken_binary ~ arm, data, se_type='HC0')
-  Meat_model = estimatr::lm_robust(meat_outcome_not_chicken_binary ~ arm, data, se_type='HC0')
-
-  # Interpretation: How many percentage points larger was the reduction in
-  # chicken selection than for other meats.
-  return(Chicken_model$coefficients['arm'] - Meat_model$coefficients['arm'])
-}
-
-
-# This works, but the BCA implementation is significantly slower than `boot`,
-# presumably because `boot` allows multicore parallelization
 H2_Specificity_CI_2 = function(data, N_bootstraps) {
-
+  
+  # Helper function that generates the point estimate of the intervention effect
+  calculate_statistics_2 = function(data) {
+    Chicken_model = estimatr::lm_robust(meat_outcome_chicken_binary ~ arm, data, se_type='HC0')
+    Meat_model = estimatr::lm_robust(meat_outcome_not_chicken_binary ~ arm, data, se_type='HC0')
+    
+    # Interpretation: How many percentage points larger was the reduction in
+    # chicken selection than for other meats.
+    return(Chicken_model$coefficients['arm'] - Meat_model$coefficients['arm'])
+  }
+  
+  # This works, but the BCA implementation is significantly slower than `boot`,
+  # presumably because `boot` allows multicore parallelization
   bca_ci = bcaboot::bcajack(data, N_bootstraps, calculate_statistics_2)
-
+  
   return(c(
     # Get the mean of the bootstrap estimates
-    H2_Specificity_point=bca_ci$B.mean[2],
+    H2_Specificity_point = bca_ci$B.mean[2],
     # Get upper and lower bounds for a 95% CI
-    H2_Specificity_CI_lo=bca_ci$lims['0.025', 'bca'],
-    H2_Specificity_CI_hi=bca_ci$lims['0.975', 'bca'])
+    H2_Specificity_CI_lo = bca_ci$lims['0.025', 'bca'],
+    H2_Specificity_CI_hi = bca_ci$lims['0.975', 'bca'])
   )
-
 }
 
+# now run the simulations
 sims = simulate_H2(5, 3332, N_bootstraps=10, H2_Specificity_CI_2,
                    arm_0_P = arm_uniform,
                    arm_1_P = arm_med_specific)
 
 print(sims)
+
 # Plot the difference between the BCA CIs and 'standard' CIs across different
 # confidence levels. Note, they are quite close.
 bcaplot(bcajack(generate_synth_data(3332, 
@@ -172,11 +127,66 @@ generate_synth_data(100000,
   group_by(arm) |>
   mutate(Percentage = Count / sum(Count))x
 
+# determine sample size
+# starting from
+bcajack(x = generate_synth_data(N = 3332, 
+                                arm_0_P = arm_uniform, 
+                                arm_1_P = arm_med_specific), 
+        500, 
+        func = calculate_statistics_2)
+
+
+#####################################
+# NON-WORKING STUFF BELOW
+
+#####################################
+# library(boot)
+# XXX Broken Still raises an cryptic error:
+# Error in bca.ci(boot.out, conf, index[1L], L = L, t = t.o, t0 = t0.o,  :
+#                  estimated adjustment 'a' is NA
+#####################################
+# Power analysis of H2 using `boot` #
+#####################################
+H2_Specificity_CI_1 = function(data, N_bootstraps) {
+  
+  # Helper function that generates the point estimate of the intervention effect
+  calculate_statistics_1 = function(data, bootstrap_sample_index) {
+    data = data[bootstrap_sample_index,]
+    
+    Chicken_model = lm_robust(meat_outcome_chicken_binary ~ arm, data,
+                              se_type = 'HC0')
+    Meat_model = lm_robust(meat_outcome_not_chicken_binary ~ arm, data,
+                           se_type = 'HC0')
+    
+    return(c(Chicken_mean = Chicken_model$coefficients['arm'],
+             Chicken_var = Chicken_model$vcov['arm','arm'],
+             
+             Meat_mean = Meat_model$coefficients['arm'],
+             Meat_var = Meat_model$vcov['arm','arm'],
+             
+             Chicken_minus_Meat = Chicken_model$coefficients['arm'] -
+               Meat_model$coefficients['arm'])
+    )
+  }
+  
+  bs = boot::boot(data, calculate_statistics_1, R=N_bootstraps, parallel='multicore')
+  
+  # Index is getting the point estimate (5) and standard error (2) via
+  # `calculate_statistics`
+  bs_ci = boot::boot.ci(bs, type="bca", index=c(5,2))
+  
+  return(c(
+    # Get upper and lower bounds for a 95% CI
+    H2_Specificity_CI_lo=bs_ci$perc[4],
+    H2_Specificity_CI_hi=bs_ci$perc[5])
+  )
+  
+}
+
+# library(boot)
+
 # error here 
 # sims = simulate_H2(N_sims=10, N_participants=3332, N_bootstraps=10,
 #                    boot_function = H2_Specificity_CI_1,
 #                    arm_0_P = arm_uniform,
 #                    arm_1_P = arm_small_specific)
-
-
-
